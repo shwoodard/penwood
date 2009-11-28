19f034371a34500a6ea8a70b91de145cf1f00d76
shwoodard <sam.h.woodard@gmail.com>
2009-11-27 17:45:23 -0800

    work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  before_filter :clear_static_assets if Rails.env.development?
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  def clear_static_assets 
+    Static::Asset.descriptor.clear
+  end
+  
 end
