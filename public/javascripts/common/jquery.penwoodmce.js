777799b96e8fef033e6c312f9b95ca9a2c3e2b76
shwoodard <sam.h.woodard@gmail.com>
2009-12-29 09:11:28 -0800

    more work on home page

---
+++ public/javascripts/common/jquery.penwoodmce.js
@@ -0,0 +1,22 @@
+(function ($) {
+  $.fn.penwoodmce = function () {
+    $(this).tinymce({
+      script_url: '/javascripts/common/tinymce/tiny_mce.js',
+      // General options
+      theme : "advanced",
+      plugins : "preview",
+
+      // Theme options
+      theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+      theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,preview,|,forecolor,backcolor",
+      theme_advanced_toolbar_location : "top",
+      theme_advanced_toolbar_align : "left",
+      theme_advanced_statusbar_location : "bottom",
+      theme_advanced_resizing : true,
+      
+      convert_fonts_to_spans : true,
+      content_css : "/stylesheets/common/global.css",
+      theme_advanced_styles: "h1=heading1,h2=heading2,p=para"
+    });
+  };
+})(jQuery);
--- public/javascripts/views/admin/contents/edit.js
+++ public/javascripts/views/admin/contents/edit.js
@@ -1,18 +1,5 @@
 (function($){
   $(document).ready(function() {
-    $('.rte').tinymce({
-      script_url: '/javascripts/common/tinymce/tiny_mce.js',
-      // General options
-      theme : "advanced",
-      // plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,imagemanager,filemanager",
-
-      // Theme options
-      theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-      theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-      theme_advanced_toolbar_location : "top",
-      theme_advanced_toolbar_align : "left",
-      theme_advanced_statusbar_location : "bottom",
-      theme_advanced_resizing : true
-    });
+    $('.rte').penwoodmce();
   });
 })(jQuery);
