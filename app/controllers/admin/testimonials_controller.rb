3a54b5ff747f7ace64d169db562fb69d174e666a
shwoodard <sam.h.woodard@gmail.com>
2009-12-29 13:10:55 -0800

    work on admin for testimonials

--- app/controllers/admin/testimonials_controller.rb
+++ app/controllers/admin/testimonials_controller.rb
@@ -1,17 +1,17 @@
 class Admin::TestimonialsController < Admin::AdminController
   def index
-     @testimonials = Testimonials.all(:order => :position)
+    @testimonials = Testimonial.all(:order => :position)
   end
-  
+
   def show
     @testimonial = Testimonial.find(params[:id])
   end
-  
+
   def new
     @testimonial = Testimonial.new
   end
-  
-  def creaqte
+
+  def create
     @testimonial = Testimonial.new(params[:testimonial])
     if @testimonial.save
       flash[:notice]  = 'You have successfully created a testimonial.'
@@ -20,11 +20,11 @@ class Admin::TestimonialsController < Admin::AdminController
       render :action => 'new'
     end
   end
-  
+
   def edit
     @testimonial = Testimonial.find(params[:id])
   end
-  
+
   def update
     @testimonial = Testimonial.find(params[:id])
     if @testimonial.update_attributes(params[:testimonial])
@@ -34,7 +34,7 @@ class Admin::TestimonialsController < Admin::AdminController
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     testimonial = Testimonial.find(params[:id])
     testimonial.destroy
