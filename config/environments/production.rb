1b7eb88984c67d448890b12ba2a7536f780d3723
shwoodard <sam.h.woodard@gmail.com>
2009-12-29 19:10:19 -0800

    add prod smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,5 +24,15 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.gmail.com",
+  :user_name  => "sam.h.woodard@gmail.com",
+  :port => 587,
+  :password  => "Buckerard02",
+  :authentication => :plain
+}
+
 # Enable threaded mode
 # config.threadsafe!
