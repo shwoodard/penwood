da4022f6acc76718d03f69f6faf457cf12dd9751
shwoodard <sam.h.woodard@gmail.com>
2010-01-02 09:59:38 -0800

    testing

--- public/javascripts/views/appointments/new.js
+++ public/javascripts/views/appointments/new.js
@@ -1,11 +1,11 @@
 (function($){
-    $(document).ready(function() {
-        $('.date_picker').datepicker();
-        $('.time_picker').ptTimeSelect();
+  $(document).ready(function() {
+      $('.date_picker').datepicker();
+      // $('.time_picker').ptTimeSelect();
         
-        $('.start.date_picker').change(function () {
-          $('.end.date_picker').val($(this).val());
-        });
+      $('.start.date_picker').change(function () {
+        $('.end.date_picker').val($(this).val());
+      });
     });
 })(jQuery);
  
