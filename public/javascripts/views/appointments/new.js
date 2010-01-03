bfb14cf868b57b973e7bf8b60c49622b04dffae2
shwoodard <sam.h.woodard@gmail.com>
2010-01-02 16:49:50 -0800

    working?

--- public/javascripts/views/appointments/new.js
+++ public/javascripts/views/appointments/new.js
@@ -1,7 +1,7 @@
 (function($){
   $(document).ready(function() {
       $('.date_picker').datepicker();
-      // $('.time_picker').ptTimeSelect();
+      $('.time_picker').ptTimeSelect();
         
       $('.start.date_picker').change(function () {
         $('.end.date_picker').val($(this).val());
