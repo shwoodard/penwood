35e5708e1cd8c0d6657d10d7d4b63e25527a18bc
shwoodard <sam.h.woodard@gmail.com>
2010-01-03 11:06:30 -0800

    removing unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :testimonials, :member => {:move => :put}
     admin.resources :users
     admin.resources :groups
-    admin.resources :image_slide_shows, :collection => {:add_image => :get}
+    admin.resources :image_slide_shows
     admin.with_options :controller => 'calendar' do |cal|
       cal.calendar 'calendar', :action => 'index', :conditions => {:method => :get}
       cal.google_call_login_callback 'calendar/do_login', :action => 'login', :conditions => {:method => :get}
