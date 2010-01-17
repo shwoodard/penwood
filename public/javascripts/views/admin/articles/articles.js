54301eb8b463f9a41cb1b0bb64bcf44f34417d82
shwoodard <sam.h.woodard@gmail.com>
2010-01-17 13:19:41 -0800

    minor tweak

--- public/javascripts/views/admin/articles/articles.js
+++ public/javascripts/views/admin/articles/articles.js
@@ -1,6 +1,6 @@
 (function ($){
   $(document).ready(function () {
-    $('select#article_type').change(function (evt) {
+    $('select#article_type').bind('change keyup', function (evt) {
       $('.article_body_row').setClass('hidden', $(this).val() !== 'MyArticle');
     });
   });
